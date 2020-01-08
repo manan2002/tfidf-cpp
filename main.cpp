539449126de01fc7cf317483285d64bf5b7eac8c
manan2002 <31039184+manan2002@users.noreply.github.com>
2020-01-08 23:10:00 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,200 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstdlib>
+#include <cstdio>
 #include "summarizer.h"
 
-int main(){
-	string s = "Pollution in Delhi is a perpetual problem which need to be looked upon as a serious issue not only by the Government but also by the citizens of the city.One of the easiest ways is that there should be an efficient involvement of Resident Welfare Associations in various localities in collection, segregation of garbage from houses and the societies.Citizens can take steps to covert the garbage into compost in their localities.More and more trees must be planted in every locality.Every individual should keep a proper check on the pollution level of their vehicles.Making more use of CNG.One of the best ways to control pollution is to manage wastes of all types in a proper manner.Each and every citizen should abide by the 3Rs: Recycle, Reuse, Reduce.More and more people should use bus and metro instead of cars and scooters, as they can carry a lot more people in one journey. Car pool is also a good option.Controlling the use of energy and making use of electricity in an efficient manner.One can also reduce water pollution by reducing the use of chemicals, cleaning agents, pesticides, herbicides, fertilizers etc.It is the duty of every citizen to think in a broader perspective to control pollution. We really don’t want our future generations to live in an unhealthy environment in Delhi. We really don’t want our children or our elders to get into incessant coughing due to pollution. Like we say charity begins at home, I take a pledge to do what I can for my environment and protect it to the best I can. If each one of us takes a pledge to do our bit for our environment, I am sure Delhi will be a better place to live in. Even asmall step counts.";
-	vector<string> sentences = getSentences(s);
-	vector<pair<int, double>> summary = getSummary(s);
-	int count = 0;
-	int numLines = 5; //Change this variable to match how many lines are needed in summary.
-	for (auto pair : summary){
-		cout << sentences[pair.first] << " --> " << pair.second << "\n\n";
-		count++;
-		if(count > numLines) break;
+using namespace std;
+
+class data{
+	string paragraph;
+	vector<pair<int, double>> summary;
+
+	vector<string> returnSentences();
+
+	void assignSummary()
+	{
+		summary = getSummary(paragraph);
+	}
+
+public:
+	static string dataFName;
+	//Constructors
+	data(string para)
+	{
+		paragraph = para;
+		assignSummary();
+	}
+	data()
+	{
+		paragraph = "Not yet set";
+	}
+
+	//Public functions.
+	vector<pair<int, double>> returnSummary();
+
+	string returnParagraph();
+
+	void displaySummary()
+	{
+		vector<string> sentences = returnSentences();
+		assignSummary();
+		int numlines = 4, count = 0;
+		cout << "Summary:\n";
+		for (auto x : summary)
+		{
+			cout << "--> " << sentences[x.first] << "\n";
+			if (count >= numlines)
+				break;
+			count++;
+		}
+		cout << "-------------------------------------------\n\n";
+	}
+
+	void displayParagraph()
+	{
+		cout << "Paragraph read from file: \n\n";
+		cout << paragraph << "\n";
+		cout << "-------------------------------------------\n\n";
+	}
+};
+
+string data::dataFName = "data.dat";
+
+vector<string> data::returnSentences()
+{
+	return getSentences(paragraph);
+}
+
+vector<pair<int, double>> data::returnSummary()
+{
+	return summary;
+}
+
+string data::returnParagraph()
+{
+	return paragraph;
+}
+
+int getNewSummary();
+void saveToFile(data d);
+void displayLogs();
+void deleteAllLogs();
+
+int main()
+{
+	remove("data.dat");
+	int choice = -1;
+	while (true)
+	{
+		do
+		{
+			system("clear");
+			cout << "\t\t Paragraph Summarizer \t\t\n\n";
+			cout << "1. Get new summary.\n";
+			cout << "2. View logs.\n";
+			cout << "3. Delete all logs.\n";
+			cout << "4. Exit.\n";
+			cout << "Enter choice : ";
+			cin >> choice;
+
+		} while (choice < 1 || choice > 4);
+		switch (choice)
+		{
+		case 1:
+			getNewSummary();
+			break;
+
+		case 2:
+			displayLogs();
+			break;
+	
+    case 3:
+        deleteAllLogs();
+        break;
+    case 4:
+        exit(0);
+        break;
+    
+		}
+	}
+	exit(0);
+}
+
+int getNewSummary()
+{
+	string Fname, textInput;
+	int choice;
+	cout << "Enter File(txt) : ";
+	cin >> Fname;
+	ifstream fin(Fname, ios::in);
+	if (!fin)
+	{
+		cout << "File doesn't exist";
+	}
+	else
+	{
+		fin.seekg(ios::beg);
+		while (fin.good())
+		{
+			getline(fin, textInput);
+		}
+
+		system("clear");
+		data d(textInput);
+		d.displayParagraph();
+		d.displaySummary();
+		cout << "1. Save summary.\n";
+		cout << "2. Cancel.\n";
+		cout << "Enter choice(1-2): ";
+		cin >> choice;
+		if (choice == 1)
+		{
+			saveToFile(d);
+		}
 	}
 	return 0;
+}
+
+void saveToFile(data d)
+{
+	ofstream fout("data.dat", ios::app | ios::binary);
+	if (fout)
+	{
+		fout.write((char *)&d, sizeof(d));
+		cout << "Written to file.\n";
+		fout.close();
+	}
+}
+
+void displayLogs()
+{
+	vector<data> logs;
+	data d;
+	ifstream fin("data.dat", ios::in | ios::binary);
+	while (fin.read((char *)&d, sizeof(d)))
+	{
+		logs.insert(logs.end(), d);
+	}
+	fin.close();
+	for (int i = 0; i < logs.size(); i++){
+		cout << i + 1 << ") \n";
+		logs[i].displayParagraph();
+		logs[i].displaySummary();
+	}
+	int n;
+	cout<<"Enter 0 when ready to go back to menu. ";
+	cin >> n;
+
+}
+
+void deleteAllLogs(){
+	remove("data.dat");
+	cout<<"All logs deleted.\n\n";
+	cout<<"Enter 0 to go back to menu. ";
+	int n;
+	cin>>n;
 }
